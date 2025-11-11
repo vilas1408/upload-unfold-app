acd422c1ed879420daa152b8d75090d6e4eef2c2
vilas1408 <chile.vilas@gmail.com>
2025-11-11 10:50:31 +0530

    Import BuyCallSpread component in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import OptionsTrading from "./pages/OptionsTrading";
 import NotFound from "./pages/NotFound";
+import { BuyCallSpread } from './components/BuyCallSpread';
 
 const queryClient = new QueryClient();
 
